ea7d14d643ac1a3c04fbf4b535da9f82fd5b7b85
U-CDW-1\amipate <amipate@LT-A0100836.corp.cdw.com>
2025-05-27 20:33:56 -0400

    Real start of the Windows WSL docker implementation and the challenges

--- run.sh
+++ run.sh
@@ -2,19 +2,28 @@
 
 #/etc/passwd:mysql:x:1001:1001:Default Application User:/home/mysql:/sbin/nologin
 #/etc/group:mysql:x:1001:
+# This version runs under WSL Ubuntu 24.04 on Windows 11
+#
 
-export CONTAINER=FalconDB
-export IMAGE=percona/percona-server:8.4
-export DATA="/opt/docker/MySql/${CONTAINER}/DATA"
-export BACKUP="/opt/docker/MySql/${CONTAINER}/BACKUP"
-export IP_EXT="10.168.10.10"
-export IP_INT="10.168.11.10"
+function check_build_dirs
+{
+	DIRS=$BACKUP $DATA $LOG
+	for d in $DIRS
+	do
+		if [ ! -d $d ]; then
+			sudo mkdir -p $d
+		fi
+done		
+sudo chown -Rv 1001:1001 $BASEDIR/$CONTAINER
+}
+
 
 function init_db
 {
 	docker stop $CONTAINER; docker rm $CONTAINER
+	check_build_dirs
 	sudo rm -rfv $DATA/*
-	sudo chown -Rv 1001:1001 $DATA ${PWD}/Dirs
+	sudo chown -Rv 1001:1001 $BASEDIR/$CONTAINER ${PWD}/Dirs
 	sudo ls -al $DATA/*
 
 	docker run \
@@ -22,7 +31,7 @@ function init_db
 		--user "1001:1001" \
 		--detach \
 		--restart on-failure \
-		--env-file falcon.env \
+		--env-file naboo.env \
 		--network external \
 		--ip $IP_EXT \
 		-p '3306:3306' \
@@ -46,9 +55,10 @@ function regular_run
 		--network external \
 		--ip $IP_EXT \
 		-p '3306:3306' \
-		-v $DATA:/var/lib/mysql \
 		-v ${PWD}/Dirs/Config:/etc/mysql \
+		-v $DATA:/var/lib/mysql \
 		-v ${BACKUP}:/BACKUP \
+		-v ${LOG}:/var/log/mysql \
 		-v ${PWD}/Dirs/bin:/tmp/bin \
 		-v /home/amitlocal/ProjectCreightonU/LOAD_CMDB.csv:/var/lib/mysql-files/LOAD_CMDB.csv \
 		-v /home/amitlocal/ProjectCreightonU:/home/mysql/ProjectCreighton  \
@@ -58,7 +68,25 @@ function regular_run
 	docker network connect --ip $IP_INT internal ${CONTAINER}
 }
 
-regular_run
+############################# MAIN #################################
+export CONTAINER=NabooDB
+export IMAGE=percona/percona-server:8.4
+export BASEDIR="/mnt/c/Users/amipate/opt/docker/MySql"
+export DATA="$BASEDIR/${CONTAINER}/DATA"
+export BACKUP="$BASEDIR/${CONTAINER}/BACKUP"
+export LOG="$BASEDIR/${CONTAINER}/LOG"
+export IP_EXT="10.168.10.10"
+export IP_INT="10.168.11.10"
+if [ -f $DATA/mysql.ibd ]; then
+	echo "Database exists"
+	regular_run
+else
+	echo "No Database"
+	init_db
+fi
+
+
+
 
 #-v ${PWD}/Dirs/Config/my.cnf:/etc/my.cnf \
 #-v /opt/docker/MySql/${CONTAINER}/DATA:/var/lib/mysql \
